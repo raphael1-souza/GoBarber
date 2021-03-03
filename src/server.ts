4ca7da8a0ee8061b616cd38acff91e3532d5d38f
raphael <raphasilvasouza96@gmail.com>
2021-03-03 15:11:35 -0300

    feat: add appointment route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-import express from 'express'
-
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello world !' })
+  return response.json({ message: 'Hello world !' });
 });
 
 app.listen(3333, () => {
-    console.log('Server Start')
+  console.log('Server Start');
 });
